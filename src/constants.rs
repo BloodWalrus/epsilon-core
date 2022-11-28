cdcfe6b18229929506bee288ded4bfdd10f0e917
BloodWalrus <github.bmo12@simplelogin.com>
2022-11-28 13:51:01 +0000

    begining refactor

---
+++ src/constants.rs
@@ -0,0 +1,4 @@
+pub const SENSOR_COUNT: usize = 7;
+pub const BONE_COUNT: usize = 9;
+pub const JOINT_COUNT: usize = 8;
+pub const CONFIG_PATH: &str = "config.toml";
--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,4 @@
 pub mod connection;
+pub mod constants;
 
 pub type EpsilonResult<T> = Result<T, Box<dyn std::error::Error>>;
